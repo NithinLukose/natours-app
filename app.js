064fc3dbc347cc838b4dffd0f120c98c96e337d5
Nithin Chacko Lukose <nithinchacko.lukose@vista.com>
2024-09-24 08:57:39 +0530

    add middlewares

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 
 const app = express();
+
+//middleware
+app.use(morgan("dev"));
 app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+// route handlers
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
@@ -16,9 +21,9 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTour = (req, res) => {
   const { id } = req.params;
 
   const tour = tours.find((tour) => tour.id === id * 1);
@@ -35,9 +40,9 @@ app.get("/api/v1/tours/:id", (req, res) => {
       message: "Invalid  id",
     });
   }
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -53,23 +58,42 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   res.status(200).json({
     status: "success",
     data: {
       tour: "placeholder",
     },
   });
-});
+};
 
-app.delete("/api/v1/tours/:id", (req, res) => {
+const deleteTour = (req, res) => {
   res.status(204).json({
     status: "success",
     data: null,
   });
-});
+};
+
+// app.get("/api/v1/tours", getAllTours);
+
+// app.get("/api/v1/tours/:id", getTour);
+
+// app.post("/api/v1/tours", createTour);
+
+// app.patch("/api/v1/tours/:id", updateTour);
+
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+//routes
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 
