9e08b0dda4d7f06aca61566a239e62aa6671b3db
Nithin Chacko Lukose <nithinchacko.lukose@vista.com>
2024-10-06 08:52:58 +0530

    mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE_CONNECTION_STRING.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => {
+  console.log("connection successful");
+});
 
 const port = process.env.PORT || 8000;
 
